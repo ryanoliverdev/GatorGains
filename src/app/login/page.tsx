2cbb37b57512960b72ef9f7390b41e48a3725c1a
ryanoliverdev <ryanoliverdev@gmail.com>
2024-02-01 17:11:25 -0500

    Added member login/signup route and login design

---
+++ src/app/login/page.tsx
@@ -0,0 +1,34 @@
+import { UserAuthForm } from "@/components/auth/user-auth-form-login";
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <div className="container my-20">
+      <div className="mx-auto flex w-full flex-col sm:p-8 sm:shadow-xl justify-center space-y-6 sm:w-[400px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Member Login
+          </h1>
+        </div>
+        <UserAuthForm />
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <Link
+            href="/terms"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Terms of Service
+          </Link>{" "}
+          and{" "}
+          <Link
+            href="/privacy"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Privacy Policy
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
+}
