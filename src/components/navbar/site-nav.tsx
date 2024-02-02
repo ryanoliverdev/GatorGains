6bf03b6561535607836137ffb1ab1bb896794dde
ryanoliverdev <ryanoliverdev@gmail.com>
2024-02-02 14:53:25 -0500

    Added site header

---
+++ src/components/navbar/site-nav.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/ui/icons";
+import { MainNav } from "@/components/navbar/main-nav";
+import { MobileNav } from "@/components/navbar/mobile-nav";
+import { buttonVariants } from "@/components/ui/button";
+
+export function SiteHeader() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        <MainNav />
+        <MobileNav />
+        <div className="flex flex-1 items-center justify-center md:justify-end">
+          <span className="sm:hidden font-bold mr-[20px]">GatorGains</span>
+        </div>
+      </div>
+    </header>
+  );
+}
