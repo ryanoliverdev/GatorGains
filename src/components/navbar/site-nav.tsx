7a6f03a16f6dff1c50df428bf6623b2af3652b24
Geon Pohl <geonpohl@Geons-MacBook-Pro.local>
2024-02-07 15:27:21 -0500

    merge master

--- src/components/navbar/site-nav.tsx
+++ src/components/navbar/site-nav.tsx
@@ -1,3 +1,4 @@
+"use server"
 import Link from 'next/link';
 
 import { cn } from '@/lib/utils';
@@ -6,19 +7,26 @@ import { MainNav } from '@/components/navbar/main-nav';
 import { MobileNav } from '@/components/navbar/mobile-nav';
 import { buttonVariants } from '@/components/ui/button';
 import { AvatarMenu } from './avatarMenu';
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions"
 
-export function SiteHeader() {
+export async function SiteHeader() {
+
+  const session = await getServerSession(authOptions)
+
+  console.log(session)
+
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
-        <MainNav />
+        <MainNav options={session} />
         <MobileNav />
         <div className="flex flex-1 items-center justify-center md:justify-end">
           <Link href="/" className="md:hidden font-bold">
             <span className="font-bold">GatorGains</span>
           </Link>
         </div>
-        <AvatarMenu />
+        <AvatarMenu options={session}/>
       </div>
     </header>
   );
