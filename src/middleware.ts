5ea9042d98db1ab5be1a9c00ce70185f46d306be
Collin Quigley <collin.quigley@ufl.edu>
2024-02-28 21:54:53 -0500

    added exercise ui

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from 'next-auth/middleware';
 
-export const config = { matcher: ['/dashboard', '/profile', '/diet'] };
+export const config = { matcher: ['/dashboard', '/profile', '/diet', '/exercises'] };
