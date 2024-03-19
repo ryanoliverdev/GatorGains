32ff12c78392e221f2f9fc30905da703959cdd52
Geon Pohl <geonpohl@Geons-MacBook-Pro.local>
2024-03-18 21:13:09 -0400

    friends api

--- src/app/api/foodLog/[userId]/route.ts
+++ src/app/api/foodLog/[userId]/route.ts
@@ -13,8 +13,10 @@ export async function POST(req: Request, { params }: { params: { userId: string 
       carbs: number;
       fat: number;
     };
+
     const today = new Date();
     const formattedDate = format(today, 'MM/dd/yyyy');
+
     const foodItem = await prisma.foodItem.upsert({
       where: { foodName },
       update: {},
@@ -46,7 +48,6 @@ export async function POST(req: Request, { params }: { params: { userId: string 
         }
       },
     });
-
     return new NextResponse(
         JSON.stringify({
           status: 'success',
@@ -57,11 +58,48 @@ export async function POST(req: Request, { params }: { params: { userId: string 
       );
   } catch (error: any) {
     return new NextResponse(
-        JSON.stringify({
-          status: 'error',
-          message: error.message
-        }),
-        { status: 500, headers: { 'Content-Type': 'application/json' } }
-      );
+      JSON.stringify({
+        status: 'error',
+        message: error.message
+      }),
+      { status: 500, headers: { 'Content-Type': 'application/json' } }
+    );
+  }
+}
+
+export async function GET(req: Request, { params }: { params: { userId: string } }, res: NextApiResponse) {
+  try {
+    const userId = params.userId as string;
+    const { date } = await req.json() as { date: string };
+    const result = await prisma.dailyFoodLog.findMany({
+      where: {
+        userId: userId,
+        date: date,
+      },
+      select: {
+        FoodEntry: {
+          select: {
+            foodItem: {
+              select: {
+                foodName: true,
+                calories: true,
+                protein: true,
+                carbs: true,
+                fat: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    return result;
+  } catch (error: any) {
+    return new NextResponse(
+      JSON.stringify({
+        status: 'error',
+        message: error.message
+      }),
+      { status: 500, headers: { 'Content-Type': 'application/json' } }
+    );
   }
 }
