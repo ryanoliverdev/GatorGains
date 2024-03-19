e760df1bbb7fe643ac73af373e2f1e29024b2fff
ryanoliverdev <ryanoliverdev@gmail.com>
2024-03-18 22:10:22 -0400

    yes it works

--- src/components/features/dashboard/page.tsx
+++ src/components/features/dashboard/page.tsx
@@ -9,7 +9,7 @@ import Image from 'next/image';
 export function DashboardFinal({ options }: { options: any }) {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1>Dashboard Change</h1>
       <p>Welcome, {options.user.name}</p>
       <p>Here&apos;s your avatar, </p>
       <img src={options.user.image} alt="react logo" />
