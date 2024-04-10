ea53edca9e85e4d260fdf8a2e36e2e7779f749b8
ryanoliverdev <ryanoliverdev@gmail.com>
2024-04-10 17:42:13 -0400

    Little cleanup

--- src/components/dietTracking/macroProgressBar.tsx
+++ src/components/dietTracking/macroProgressBar.tsx
@@ -7,7 +7,7 @@ export default function MacroProgress({
   type,
   dailyCals,
   macroInfo,
-  totalEaten,
+  totalEaten
 }: {
   amount: number;
   type: string;
@@ -15,18 +15,21 @@ export default function MacroProgress({
   macroInfo: any;
   totalEaten: number;
 }) {
-    console.log(macroInfo.dailyProteinPercentage)
-    console.log((((amount * 4) * 100) / ((macroInfo.dailyProteinPercentage/100) * dailyCals)))
   if (type === 'Protein') {
     return (
       <div className="flex gap-2 ">
         <h1 className="text-sm mr-1">Protein</h1>
         <Progress
           indicatorColor=" bg-green-400"
-          value={Math.min((((amount * 4) * 100) / ((macroInfo.dailyProteinPercentage/100) * dailyCals)), 100)}
+          value={Math.min(
+            (amount * 4 * 100) /
+              ((macroInfo.dailyProteinPercentage / 100) * dailyCals),
+            100
+          )}
         />
         <h1 className="text-sm w-[150px] text-right">
-        {(((amount * 4) / dailyCals) * 100).toFixed(1)}% / {macroInfo.dailyProteinPercentage}%
+          {(((amount * 4) / dailyCals) * 100).toFixed(1)}% /{' '}
+          {macroInfo.dailyProteinPercentage}%
         </h1>
       </div>
     );
@@ -36,10 +39,15 @@ export default function MacroProgress({
         <h1 className="text-sm mr-3">Carbs</h1>
         <Progress
           indicatorColor="bg-red-400"
-          value={Math.min((((amount * 4) * 100) / ((macroInfo.dailyCarbsPercentage/100) * dailyCals)), 100)}
+          value={Math.min(
+            (amount * 4 * 100) /
+              ((macroInfo.dailyCarbsPercentage / 100) * dailyCals),
+            100
+          )}
         />
         <h1 className="text-sm w-[150px] text-right">
-        {(((amount * 4) / dailyCals) * 100).toFixed(1)}% / {macroInfo.dailyCarbsPercentage}%
+          {(((amount * 4) / dailyCals) * 100).toFixed(1)}% /{' '}
+          {macroInfo.dailyCarbsPercentage}%
         </h1>
       </div>
     );
@@ -49,10 +57,15 @@ export default function MacroProgress({
         <h1 className="text-sm mr-7">Fat</h1>
         <Progress
           indicatorColor="bg-yellow-200"
-          value={Math.min((((amount * 9) * 100) / ((macroInfo.dailyFatPercentage/100) * dailyCals)), 100)}
+          value={Math.min(
+            (amount * 9 * 100) /
+              ((macroInfo.dailyFatPercentage / 100) * dailyCals),
+            100
+          )}
         />
         <h1 className="text-sm w-[150px] text-right">
-        {(((amount * 9) / dailyCals) * 100).toFixed(1)}% / {macroInfo.dailyFatPercentage}%
+          {(((amount * 9) / dailyCals) * 100).toFixed(1)}% /{' '}
+          {macroInfo.dailyFatPercentage}%
         </h1>
       </div>
     );
